6d92cbbe654d03db724b69ad3e606a3a5d38032b
Ivan Mosiev <i.k.mosev@gmail.com>
2013-03-22 03:26:59 +0200

    Added mysql code

---
+++ setup_db.php
@@ -0,0 +1,10 @@
+<?php
+require_once './dbConnect.php';
+
+    $create_sql = "CREATE TABLE registration_tbl(
+        id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id),
+        name VARCHAR(30),
+        email VARCHAR(30),
+        date DATE)";
+    $conn->exec($create_sql);
