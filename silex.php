11da6fde8de0b136438ec54bc9cd12bdf1f31a54
Ivan Mosiev <i.k.mosev@gmail.com>
2013-03-24 23:14:30 +0200

    And how is silex works on site?

---
+++ silex.php
@@ -0,0 +1,10 @@
+<?php
+include_once('vendor/autoload.php');
+
+$app = new Silex\Application();
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
