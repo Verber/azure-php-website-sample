b92ec9f2fff90b3d6f4a305158aadb17af0c7125
Ivan Mosiev <i.k.mosev@gmail.com>
2013-03-26 02:48:04 +0200

    Added info route

--- silex.php
+++ silex.php
@@ -2,14 +2,12 @@
 include_once('vendor/autoload.php');
 ini_set('display_errors', 1);
 
-try {
-    $app = new Silex\Application();
+$app = new Silex\Application();
 
-    $app->get('/blobs/', 'Demos\Blobs::index');
-    $app->post('/blobs/upload', 'Demos\Blobs::upload');
-    $app['debug'] = true;
+$app->get('/blobs/', 'Demos\Blobs::index');
+$app->post('/blobs/upload', 'Demos\Blobs::upload');
 
-    $app->run();
-} catch (Exception $e) {
-    die($e->getMessage());
-}
+$app->get('/info/', phpinfo());
+$app['debug'] = true;
+
+$app->run();
